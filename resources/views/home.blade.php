a356a0d59c595b83c92afae0afaa6ffcb5a8159d
Your Name <you@example.com>
2023-10-02 12:07:17 -0400

    actualizacion Navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,49 +12,49 @@
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-    @include('layout.navbarUser')
-        <div class="container">
-            <!-- Barra de navegacion-->
+@include('layout.navbarProy')
+<div class="container">
+    <!-- Barra de navegacion-->
 
-            <!--Seccion de Bienvenida -->
-            <div class="welcome-section">
-                <div class="header-container">
-                    <h2>CrowdFunding</h2>
-                </div>
-                <div class="text-container">
-                    <p class="text-section">Apoya a los proyectos de reforestacion de Bolivia</p>
-                </div>
-            </div>
-            <!-- Mision y arboles plantados -->
-            <div class="secciones">
+    <!--Seccion de Bienvenida -->
+    <div class="welcome-section">
+        <div class="header-container">
+            <h2>CrowdFunding</h2>
+        </div>
+        <div class="text-container">
+            <p class="text-section">Apoya a los proyectos de reforestacion de Bolivia</p>
+        </div>
+    </div>
+    <!-- Mision y arboles plantados -->
+    <div class="secciones">
 
-                <div class="donados">
-                    <div class="donados-container">
-                        <h2 class="header-arboles">Arboles donados</h2>
-                        <h3 class="nro-arboles">1.024</h3>
-                        <p class="msg">Ayuda a reforestar Santa Cruz.</p>
-                        <p class="msg2">
-                                Los árboles garantizan que el aire, el agua y los suelos se mantengan limpios y sanos.
-                                Cada uno de nosotros puede contribuir:
-                                elige cómo quieres actuar y salvaguardar tu futuro.
-                        </p>
-                    </div>
-                </div>
-                <div class="mision">
-                    <div class="image-div"></div>
-                    <!--<div class="mision-div">
-                        <h2 class="header-mision">Nuestra Mision</h2>
-                        <p class="body-mision">
-                            Apoyar a todas las iniciativas de reforestacion en el departamento de Santa Cruz,
-                            otorgando la facilidad de apoyar estos proyectos.
-                        </p>
-                        <p>
-
-                        </p> -->
-                    </div>
-                </div>
+        <div class="donados">
+            <div class="donados-container">
+                <h2 class="header-arboles">Arboles donados</h2>
+                <h3 class="nro-arboles">1.024</h3>
+                <p class="msg">Ayuda a reforestar Santa Cruz.</p>
+                <p class="msg2">
+                    Los árboles garantizan que el aire, el agua y los suelos se mantengan limpios y sanos.
+                    Cada uno de nosotros puede contribuir:
+                    elige cómo quieres actuar y salvaguardar tu futuro.
+                </p>
             </div>
         </div>
+        <div class="mision">
+            <div class="image-div"></div>
+            <!--<div class="mision-div">
+                <h2 class="header-mision">Nuestra Mision</h2>
+                <p class="body-mision">
+                    Apoyar a todas las iniciativas de reforestacion en el departamento de Santa Cruz,
+                    otorgando la facilidad de apoyar estos proyectos.
+                </p>
+                <p>
+
+                </p> -->
+        </div>
+    </div>
+</div>
+</div>
 
 
 </body>
