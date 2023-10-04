56b4792959cc04a4ef0597423d9401a9ccd80641
Your Name <you@example.com>
2023-10-04 17:02:02 -0400

    correcion de la pagina principal

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/quienessomos', function () {
+    return view('contribuidores/view');
+});
 
 
