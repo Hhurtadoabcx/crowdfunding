904fa6586c387b9e06d2347806dec5a74b0470af
Hhurtadoabcx <contacthurtadolxyz@gmail.com>
2023-10-09 10:46:01 -0400

    Indexaciones

---
+++ resources/views/contribuidores/view.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nosotros</title>
+
+    <link rel="stylesheet" href="/css/contribuitors-views.css">
+</head>
+<body>
+@include('layout.navbarProy')
+
+<div class="container">
+    <div class="contribuidores" style="background-image: linear-gradient(to bottom, #1b7161, #1b7161);">
+        <div class="container">
+            <!-- Líderes -->
+            <div class="lideres">
+                <div class="card">
+                    <img src="https://generated.photos/vue-static/face-generator/landing/wall/1.jpg" alt="Foto del contribuidor" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Juan Pérez</h5>
+                        <p class="card-text">Product Owner</p>
+                        <p class="card-text">juan@email.com</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://generated.photos/vue-static/face-generator/landing/wall/2.jpg" alt="Foto del contribuidor" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">María Rodríguez</h5>
+                        <p class="card-text">SCRUM Master</p>
+                        <p class="card-text">maria@email.com</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Desarrolladores -->
+            <div class="desarrolladores">
+                <div class="card">
+                    <img src="https://generated.photos/vue-static/face-generator/landing/wall/3.jpg" alt="Foto del contribuidor" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Juan Gómez</h5>
+                        <p class="card-text">Desarrollador</p>
+                        <p class="card-text">juang@email.com</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://generated.photos/vue-static/face-generator/landing/wall/4.jpg" alt="Foto del contribuidor" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Pedro López</h5>
+                        <p class="card-text">Desarrollador</p>
+                        <p class="card-text">pedro@email.com</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://generated.photos/vue-static/face-generator/landing/wall/5.jpg" alt="Foto del contribuidor" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Laura Torres</h5>
+                        <p class="card-text">Desarrollador</p>
+                        <p class="card-text">laura@email.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
